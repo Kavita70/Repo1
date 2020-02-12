9211f15e58a9c7fa1f914f1a7977fdf5aea195df
Kavita70 <41122890+Kavita70@users.noreply.github.com>
2020-02-12 12:40:19 +0000

    Add files via upload

---
+++ number_class.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 12:37:29 2020
+
+@author: ShubhamBhatnagar
+"""
+
+# test python file
+
+    
+
+while True:
+    num = int(input('Enter a number: '))
+    if num < 20:
+        print('Too small!')
+    elif num > 50:
+        print('Too big!')
+    else:
+        print('Good number')
+        break
